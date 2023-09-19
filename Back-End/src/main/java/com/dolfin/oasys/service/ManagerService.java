b68a16e3d1199c1e099492c1fc0f2a83ec02e7d9
mj <aud19980518@gmail.com>
2023-09-19 14:04:07 +0900

    Feat: redis, mariadb configuration file updated
    redis, mariadb configuration file updated.

--- Back-End/src/main/java/com/dolfin/oasys/service/ManagerService.java
+++ Back-End/src/main/java/com/dolfin/oasys/service/ManagerService.java
@@ -1,4 +1,19 @@
 package com.dolfin.oasys.service;
 
+import com.dolfin.oasys.model.dto.MemberDto;
+import com.dolfin.oasys.model.dto.TellerStatusDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.List;
+
 public interface ManagerService {
+    List<TellerStatusDTO> getTellerStatusList();
+
+    void addConsumerToConsultation(MemberDto.RequestMember requestMember) throws JsonProcessingException;
+
+    void completeConsultation(Long tellerType);
+
+    MemberDto.ResponseMember getMemberInfoByFaceId(Long TellerTypeId, int count) throws JsonProcessingException;
+
+    void createMember(MemberDto.RequestNewMember requestNewMember);
 }
