4ab0c28bdb1b9b35e212e469c08747b8f31bb14c
Crassula1994 <gogun3535@gmail.com>
2023-09-19 17:38:09 +0900

    Feat: Add router

--- frontend/oasys/src/pages/admin/Admin.tsx
+++ frontend/oasys/src/pages/admin/Admin.tsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function Admin() {
-    return <div>관리자 페이지</div>;
+    return (
+        <div>
+            관리자 페이지
+            <Outlet />
+        </div>
+    );
 }
 
 export default Admin;
--- frontend/oasys/src/pages/senior/Senior.tsx
+++ frontend/oasys/src/pages/senior/Senior.tsx
@@ -1,5 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function Senior() {
-    return <div>시니어 페이지</div>;
+    return (
+        <div>
+            시니어 페이지
+            <Outlet />
+        </div>
+    );
 }
 
 export default Senior;
