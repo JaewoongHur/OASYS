c1c84a99460cf0c716241fc3d19d780d67b872b7
김동현 <dhkim960511@naver.com>
2023-09-20 14:40:56 +0900

    Refactor: Delete entity

--- Back-End/src/main/java/com/dolfin/oasys/domain/member/model/entity/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.dolfin.oasys.domain.member.model.entity;
-
-public enum Gender {
-    MALE,FEMALE;
-}
--- Back-End/src/main/java/com/dolfin/oasys/domain/member/model/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dolfin.oasys.domain.member.model.repository;
-
-import com.dolfin.oasys.domain.member.model.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-    Member findByFaceId(String faceId);
-}
