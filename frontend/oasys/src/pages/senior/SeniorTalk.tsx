11380b1b1675ee06e752fcf49d02c3b9b8ddf403
Crassula1994 <gogun3535@gmail.com>
2023-09-22 14:15:00 +0900

    Feat: Add face recognition with webcam

--- frontend/oasys/src/pages/senior/SeniorTalk.tsx
+++ frontend/oasys/src/pages/senior/SeniorTalk.tsx
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useSpeechRecognition } from 'react-speech-kit';
-import axios from 'axios';
-import './SeniorTalk.css';
+import { useState, useEffect } from "react";
+import { useSpeechRecognition } from "react-speech-kit";
+import axios from "axios";
+import "./SeniorTalk.css";
 import styled from "@emotion/styled";
 import Footer from "@/components/common/footer/Footer";
 import BackgroundImage from "@/assets/images/background image.png";
-import gifWoman from './oasys_woman1.gif'; 
-
-
-interface IResponse {
-  data: string;
-}
+import gifWoman from "./oasys_woman1.gif";
 
 const MainImg = styled("div")`
     background-image: url(${BackgroundImage});
@@ -21,104 +16,104 @@ const MainImg = styled("div")`
 `;
 
 function SeniorTalk() {
-  const [value, setValue] = useState<string>('');
-  const [isRecording, setIsRecording] = useState<boolean>(false);
-  const [lastSpeechTime, setLastSpeechTime] = useState<number | null>(null);
+    const [value, setValue] = useState<string>("");
+    const [isRecording, setIsRecording] = useState<boolean>(false);
+    const [lastSpeechTime, setLastSpeechTime] = useState<number | null>(null);
 
-  const handleMic = () => {
-      setIsRecording(!isRecording);
-  };
+    const handleMic = () => {
+        setIsRecording(!isRecording);
+    };
 
-  const sendToBackend = async (text: string) => {
-    try {
-      const response = await axios.post<string>("http://localhost:8081/gpt/voice", { text }, {
-        headers: {
-          'Content-Type': 'application/json'
+    const sendToBackend = async (text: string) => {
+        try {
+            const response = await axios.post<string>(
+                "http://localhost:8081/gpt/voice",
+                { text },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                },
+            );
+            const receivedText = response.data;
+
+            setValue(receivedText);
+            // textToSpeech(receivedText);
+        } catch (error) {
+            console.error("Error sending voice text to backend:", error);
         }
-      });
-      const receivedText = response.data;
+    };
 
-      setValue(receivedText);
-      // textToSpeech(receivedText);
-    } catch (error) {
-      console.error("Error sending voice text to backend:", error);
-    }
-  };
+    const { listen, stop } = useSpeechRecognition({
+        onResult: (result) => {
+            setValue(result);
+            setLastSpeechTime(Date.now());
+        },
+    });
 
-  const { listen, listening, stop } = useSpeechRecognition({
-    onResult: (result) => {
-      setValue(result);
-      setLastSpeechTime(Date.now());
-    },
-  });
+    useEffect(() => {
+        if (isRecording && lastSpeechTime) {
+            const checkSilenceInterval = setInterval(() => {
+                if (Date.now() - lastSpeechTime > 3000) {
+                    stop();
+                    setIsRecording(false);
+                    sendToBackend(value);
+                    setLastSpeechTime(null); // Reset the last speech time when silence is detected
+                }
+            }, 1000);
 
-  useEffect(() => {
-    if (isRecording && lastSpeechTime) {
-        const checkSilenceInterval = setInterval(() => {
-            if (Date.now() - lastSpeechTime > 3000) {
-                stop();
-                setIsRecording(false);
-                sendToBackend(value);
-                setLastSpeechTime(null);  // Reset the last speech time when silence is detected
-            }
-        }, 1000);
+            return () => clearInterval(checkSilenceInterval);
+        }
+    }, [isRecording, lastSpeechTime, stop]);
 
-        return () => clearInterval(checkSilenceInterval);
-    }
-}, [isRecording, lastSpeechTime, stop]);
+    const textToSpeech = (text: string) => {
+        if ("speechSynthesis" in window) {
+            const synth = window.speechSynthesis;
+            let voices = synth.getVoices();
+            voices = voices.filter((voice) => voice.lang.includes("ko"));
+            console.log(voices);
+            const utterance = new SpeechSynthesisUtterance(text);
+            utterance.voice = voices[1];
+            synth.speak(utterance);
+        } else {
+            console.warn("Your browser does not support text to speech.");
+        }
+    };
 
+    const toggleRecording = () => {
+        if (isRecording) {
+            stop();
+            setIsRecording(false);
+            setLastSpeechTime(null); // Reset the last speech time when stopping
+        } else {
+            listen();
+            setIsRecording(true);
+        }
+    };
 
-  const textToSpeech = (text: string) => {
-    if ('speechSynthesis' in window) {
-      const synth = window.speechSynthesis;
-      let voices = synth.getVoices();
-      voices = voices.filter(voice => voice.lang.includes('ko'));
-      console.log(voices);
-      const utterance = new SpeechSynthesisUtterance(text);
-      utterance.voice = voices[1];
-      synth.speak(utterance);
-    } else {
-      console.warn("Your browser does not support text to speech.");
-    }
-  };
-
-  const toggleRecording = () => {
-    if (isRecording) {
-        stop();
-        setIsRecording(false);
-        setLastSpeechTime(null);  // Reset the last speech time when stopping
-    } else {
-        listen();
-        setIsRecording(true);
-    }
-};
-
-
-  return (
-    <div className="seniorTalkContainer">
-      <img src={gifWoman} alt="Description of GIF" className="leftGif" />
-      <div><span className="answerText">{value}</span></div>
-      <button className="btnRecord" onClick={toggleRecording}>
-          {isRecording ? "음성 인식 중입니다 🎧" : "말하기 💬"}
-      </button>
-      <div className="ocean">
-        <div className="wave"></div>
-        <div className="wave"></div>
+    return (
+        <div className="seniorTalkContainer">
+            <img src={gifWoman} alt="Description of GIF" className="leftGif" />
+            <div>
+                <span className="answerText">{value}</span>
+            </div>
+            <button type="button" className="btnRecord" onClick={toggleRecording}>
+                {isRecording ? "음성 인식 중입니다 🎧" : "말하기 💬"}
+            </button>
+            <div className="ocean">
+                <div className="wave" />
+                <div className="wave" />
+            </div>
+            <MainImg>
+                {/* <img src={AIwoman} width="25%" height="100%" alt="AI woman" /> */}
+                <Footer
+                    backgroundColor={!isRecording ? "#222222" : "#E5552F"}
+                    text={!isRecording ? "잠시만 기다려주세요." : "지금 말씀해보세요."}
+                    onClick={handleMic}
+                />
+            </MainImg>
         </div>
-        <MainImg>
-            {/* <img src={AIwoman} width="25%" height="100%" alt="AI woman" /> */}
-            <Footer
-                backgroundColor={!isRecording ? "#222222" : "#E5552F"}
-                text={!isRecording ? "잠시만 기다려주세요." : "지금 말씀해보세요."}
-                onClick={handleMic}
-            />
-        </MainImg>
-    </div>
-    
-    
-);
-
-
+    );
 }
 
 export default SeniorTalk;
