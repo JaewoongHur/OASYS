568b6b3eb8fe6cf8bd436fa473c5b1eb391356a6
Jaewoong Hur <jwhur7@naver.com>
2023-10-04 07:04:14 +0900

    Feat: Add dropdown component

--- frontend/oasys/src/assets/icons/index.ts
+++ frontend/oasys/src/assets/icons/index.ts
@@ -3,9 +3,9 @@ import Bankbook from "./bankbook-icon.svg";
 import Card from "./card-icon.svg";
 import Internet from "./internet-icon.svg";
 import Money from "./money-icon.svg";
-import ArrowUp from "./arrow up.svg";
-import ArrowDown from "./arrow down.svg";
+import RightArrow from "./right arrow.svg";
+
 // ----------------------------------------------------------------------------------------------------
 
 /* Export */
-export { Bankbook, Card, Internet, Money, ArrowUp, ArrowDown };
+export { Bankbook, Card, Internet, Money, RightArrow };
--- frontend/oasys/src/components/common/dropdown/Dropdown.tsx
+++ frontend/oasys/src/components/common/dropdown/Dropdown.tsx
@@ -1,22 +1,76 @@
 /* import */
-import React, { useState } from "react";
+import { useState } from "react";
 import styled from "@emotion/styled";
 import Modal from "@components/modal/Modal";
-import { ArrowUp, ArrowDown } from "@/assets/icons";
-import { TextInput } from "../input";
+import { RightArrow } from "@/assets/icons";
 
 // ----------------------------------------------------------------------------------------------------
 /* Props Type */
-interface DropdownProps {
+interface DropdownProps extends DropdownInputProps {
     placeholder: string;
     optionList: { name: string; value: number }[];
 }
+interface ArrowImageProps {
+    rotate: boolean; // 사용자 지정 속성으로 선언
+}
+interface DropdownInputProps {
+    width: string;
+    height: string;
+}
+
 // ----------------------------------------------------------------------------------------------------
 /* Style */
 const DropDownContainer = styled("div")`
     margin-top: 100px;
     margin-left: 100px;
     cursor: pointer;
+`;
+
+const InputContainer = styled.div<DropdownInputProps>`
+    // Size Attribute
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    padding: 10px;
+    box-sizing: border-box;
+
+    // Style Attribute
+    background-color: ${(props) => props.theme.colors.gray1};
+    border: 5px solid ${(props) => props.theme.colors.primary3};
+    border-radius: 20px;
+
+    // Text Attribute
+    color: ${(props) => props.theme.colors.gray7};
+    font-weight: 700;
+
+    // Interaction Attribute
+    transition: 0.3s;
+    outline: none;
+    :focus {
+        box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.5);
+    }
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const DropDownInput = styled.input`
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    font-size: 16px;
+    padding: 8px;
+    cursor: pointer;
+`;
+
+const ArrowImage = styled("img")<ArrowImageProps>`
+    width: 20px;
+    height: 20px;
+    margin-left: 10px;
+    transition: transform 0.3s ease;
+    transform: rotate(${(props) => (props.rotate ? "90deg" : "0deg")});
 `;
 
 const Option = styled("button")`
@@ -28,7 +82,6 @@ const Option = styled("button")`
 
     &:hover {
         border-radius: 20px;
-        /* background-color: black; */
         background-color: ${(props) => props.theme.colors.gray1};
         cursor: pointer;
     }
@@ -37,34 +90,30 @@ const Option = styled("button")`
 // ----------------------------------------------------------------------------------------------------
 /* Dropdown Component */
 function Dropdown(props: DropdownProps) {
-    const { placeholder, optionList } = props;
-    const [arrowIcon, setArrowIcon] = useState<boolean>(false);
+    const { placeholder, optionList, width, height } = props;
+    const [showOptions, setShowOptions] = useState<boolean>(false);
     const [selectedOption, setSelectedOption] = useState<string>("");
 
-    const toggleButton = () => {
-        setArrowIcon(!arrowIcon);
+    const toggleOptions = () => {
+        setShowOptions(!showOptions);
     };
 
     const onClickOption = (name) => {
         setSelectedOption(name);
-        setArrowIcon(false);
+        setShowOptions(false);
     };
 
     return (
         <DropDownContainer>
-            <TextInput
-                readOnly
-                width="200px"
-                value={selectedOption}
-                placeholder={placeholder}
-                inputIconSrc={arrowIcon ? ArrowUp : ArrowDown}
-                onClick={toggleButton}
-            />
-            {arrowIcon && (
+            <InputContainer width={width} height={height} onClick={toggleOptions}>
+                <DropDownInput readOnly value={selectedOption} placeholder={placeholder} />
+                <ArrowImage src={RightArrow} alt="Arrow Icon" rotate={showOptions} />
+            </InputContainer>
+            {showOptions && (
                 <Modal
                     width="200px"
                     height="100px"
-                    openModal={arrowIcon}
+                    openModal={showOptions}
                     posX="100px"
                     posY="-40px"
                     overlayOn={false}
@@ -84,6 +133,7 @@ function Dropdown(props: DropdownProps) {
         </DropDownContainer>
     );
 }
+
 // ----------------------------------------------------------------------------------------------------
 
 /* Export */
