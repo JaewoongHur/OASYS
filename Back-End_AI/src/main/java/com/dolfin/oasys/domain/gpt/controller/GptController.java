50b4411d591c6899a7eab84ccd81fb7af829b084
정연수 <jos6151@naver.com>
2023-10-04 14:29:20 +0900

    Feat: Add GPT fine tuning data

--- Back-End_AI/src/main/java/com/dolfin/oasys/domain/gpt/controller/GptController.java
+++ Back-End_AI/src/main/java/com/dolfin/oasys/domain/gpt/controller/GptController.java
@@ -1,106 +1,50 @@
 package com.dolfin.oasys.domain.gpt.controller;
 
-import com.dolfin.oasys.domain.gpt.mp3.PlayerMP3;
-import com.dolfin.oasys.domain.stt.controller.VideoController;
-import com.google.cloud.texttospeech.v1.*;
-import com.google.protobuf.ByteString;
-import com.theokanning.openai.completion.CompletionRequest;
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
-import com.theokanning.openai.completion.chat.ChatMessageRole;
-import com.theokanning.openai.service.OpenAiService;
-import com.twilio.twiml.voice.Play;
+import com.dolfin.oasys.domain.gpt.service.GPTService;
 import lombok.RequiredArgsConstructor;
-import org.python.util.PythonInterpreter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/gpt")
+@RequestMapping("/api/v1/voice")
 @RequiredArgsConstructor
 public class GptController {
-    @Value("${gpt.api.key}")
-    private String key;
 
 
-    private OpenAiService service;
+    private final GPTService gptService;
 
-    @PostConstruct
-    public void connectGPT(){
-        this.service = new OpenAiService(key);
+    @PostMapping("/test")
+    public ResponseEntity<?> testGPT(String question){
+        gptService.sendQuestion(question);
+        return ResponseEntity.ok(gptService.sendQuestion(question));
     }
 
+    @PostMapping("/question")
+    public ResponseEntity<String> receiveVoiceText(@RequestBody Map<String, Object> requestData) {
+        String voiceText = (String)requestData.get("text");
+        String gender = (String)requestData.get("gender");
+        System.out.println(gender);
+        String answerText= "";
+        answerText=gptService.taskQuestion(voiceText,gender);
 
-    private PythonInterpreter py;
-
-    /*resource\\mp3 폴더의 절대 경로 넣기*/
-   private final String FILE_PATH = "./src/main/resources/mp3/";
-
-
-    @PostMapping("/voice")
-    public ResponseEntity<String> receiveVoiceText(@RequestBody Map<String, String> voiceData) {
-        String voiceText = voiceData.get("text");
-        final List<ChatMessage> messages = new ArrayList<>();
-        final ChatMessage systemMessage2 = new ChatMessage(ChatMessageRole.SYSTEM.value(), "1. 인출 2. 입금 3. 송금 4.대출 상담 이라는 선택지가 있어. 앞으로 내가 하는 말이 어디에 속하는지 단답으로ㅎ 알려줘.");
-        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(),  voiceText);
-        messages.add(systemMessage2);
-        messages.add(systemMessage);
-
-
-        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-                .builder()
-                .model("gpt-3.5-turbo")
-                .messages(messages)
-                .n(1)
-                .maxTokens(10)
-                .logitBias(new HashMap<>())
-                .build();
-        ChatMessage responseMessage = service.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage();
-
-        String answerText = responseMessage.getContent();
-        System.out.println((answerText));
-//        gptService.sendToDesk("female");
-        PlayerMP3 receive;
-        if(answerText.contains("인출")||answerText.contains("1")) {
-            receive = new PlayerMP3(FILE_PATH + "인출_접수_여자.mp3");
-            answerText = "인출 업무 접수 완료";
-            receive.playing();
-        }
-        else if(answerText.contains("입금")||answerText.contains("2")) {
-            receive = new PlayerMP3(FILE_PATH+ "입금_접수_여자.mp3");
-            answerText = "입금 업무 접수 완료";
-            receive.playing();
-        }
-        else if(answerText.contains("송금")||answerText.contains("3")) {
-            receive = new PlayerMP3(FILE_PATH + "송금_접수_여자.mp3");
-            answerText = "송금 업무 접수 완료";
-            receive.playing();
-        }
-
-        /*추후 입력받은 업무를 확인하고, 해당 변수를 던지는 과정이 추가될 자리*/
-        try{
-        Thread.sleep(3000);}
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        PlayerMP3 playerMP3 = new PlayerMP3(FILE_PATH+"접수_확인_여자.mp3");
-
-        playerMP3.playing();
+        System.out.println(gptService.getTaks());
         return ResponseEntity.ok(answerText);
+    }
+
+    @PostMapping("/confirm")
+    public ResponseEntity<String> confrimTask(@RequestBody Map<String,Object> requestData){
+        String voiceText = (String)requestData.get("text");
+        String gender = (String)requestData.get("gender");
+        System.out.println(gender);
+        String answerText= "";
+        answerText = gptService.confirmTask(voiceText,gender);
+        return ResponseEntity.ok(answerText);
+
     }
 
 
---
+++ Back-End_AI/src/main/java/com/dolfin/oasys/domain/gpt/service/GPTService.java
@@ -0,0 +1,189 @@
+package com.dolfin.oasys.domain.gpt.service;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.fine_tuning.FineTuningJobRequest;
+import com.theokanning.openai.service.OpenAiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GPTService {
+
+
+
+    @Value("${gpt.api.key}")
+    private String key;
+
+    /*resource\\mp3 폴더의 절대 경로 넣기*/
+    private final String filePath = "./src/main/resources/mp3/";
+
+    private boolean confirm=false;
+
+    private OpenAiService service;
+
+    @PostConstruct
+    public void connectGPT(){
+        this.service = new OpenAiService(key);
+
+    }
+
+    private String task="";
+    public void createFineTuning(){
+        FineTuningJobRequest fineTuningJobRequest = new FineTuningJobRequest();
+        fineTuningJobRequest.setTrainingFile("file-vAgSFC7Lg1EZXm9Ftq2u2SWK");
+        fineTuningJobRequest.setModel("gpt-3.5-turbo");
+
+        service.createFineTuningJob(fineTuningJobRequest);
+
+    }
+    public Object sendQuestion(String question){
+
+        String answerText="";
+        final List<ChatMessage> messages = new ArrayList<>();
+        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.USER.value(), question);
+        messages.add(systemMessage);
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model("ft:gpt-3.5-turbo-0613:personal::85nuYF05")
+                .messages(messages)
+                .maxTokens(100)
+                .n(1)
+                .build();
+        return service.createChatCompletion(chatCompletionRequest).getChoices();
+
+    }
+
+ /*
+ * 은행 업무'
+ * 자주 사용하는 서비스 - 입금, 계좌 이체, 송금, 출금, 통장 정리, 현금 서비스, 공과금 납부
+ *
+ 통장, 계좌 창구 - 입금, 계좌 이체, 송금, 출금, 통장 정리 등
+
+카드 창구 - 카드 발급 및 재발급, 한도 및 비밀변호 변경, 신고, 현금 서비스 등
+
+인터넷 뱅킹 - 가입, OTP 및 보안 카드 업무, 텔레 뱅킹, 이체한도 및 비밀번호 변경 등
+
+대출, 외환 창구 - 예적금 담보 대출, 원금 및 이자 상환, 공과금 납부, 증명서 발금, 환전 등
+ *
+ *
+ * */
+    public String taskQuestion(String voiceText, String gender){
+        //
+        String answerText = "";
+        final List<ChatMessage> messages = new ArrayList<>();
+        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), voiceText);
+        messages.add(systemMessage);
+
+
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                .builder()
+                .model("ft:gpt-3.5-turbo-0613:personal::85nuYF05")
+                .messages(messages)
+                .n(1)
+                .maxTokens(10)
+                .logitBias(new HashMap<>())
+                .build();
+        ChatMessage responseMessage = service.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage();
+        answerText = responseMessage.getContent();
+        PlayerMP3 receive;
+        if (gender.equals("MALE")) {
+            if (answerText.contains("인출") || answerText.contains("1")) {
+                receive = new PlayerMP3(filePath + "인출_확인_여자.mp3");
+                answerText = "인출 업무 확인";
+                task="인출 업무";
+                receive.playing();
+            } else if (answerText.contains("입금") || answerText.contains("2")) {
+                receive = new PlayerMP3(filePath + "입금_확인_여자.mp3");
+                answerText = "입금 업무 확인";
+                task="입금 업무";
+                receive.playing();
+            } else if (answerText.contains("송금") || answerText.contains("3")) {
+                receive = new PlayerMP3(filePath + "송금_확인_여자.mp3");
+                answerText = "송금 업무 확인";
+                task="송금 업무";
+                receive.playing();
+            }
+        } else {
+            if (answerText.contains("인출") || answerText.contains("1")) {
+                receive = new PlayerMP3(filePath + "인출_확인_남자.mp3");
+                answerText = "인출 업무 확인";
+                task="인출 업무";
+                receive.playing();
+            } else if (answerText.contains("입금") || answerText.contains("2")) {
+                receive = new PlayerMP3(filePath + "입금_확인_남자.mp3");
+                answerText = "입금 업무 확인";
+                task="입금 업무";
+                receive.playing();
+            } else if (answerText.contains("송금") || answerText.contains("3")) {
+                receive = new PlayerMP3(filePath + "송금_확인_남자.mp3");
+                answerText = "송금 업무 확인";
+                task="송금 업무";
+                receive.playing();
+            }
+        }
+        return answerText;
+    }
+
+    public String confirmTask(String voiceText,String gender){
+        String answerText="";
+        final List<ChatMessage> messages = new ArrayList<>();
+        final ChatMessage systemMessage2 = new ChatMessage(ChatMessageRole.SYSTEM.value(), "이게 긍정의 대답이면 '네', 아니면 '아니오'를 출력해줘");
+        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), voiceText);
+        messages.add(systemMessage2);
+        messages.add(systemMessage);
+
+
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-3.5-turbo")
+                .messages(messages)
+                .n(1)
+                .maxTokens(10)
+                .logitBias(new HashMap<>())
+                .build();
+        ChatMessage responseMessage = service.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage();
+        answerText = responseMessage.getContent();
+        System.out.println(answerText);
+        PlayerMP3 receive;
+        if(gender.equals("MALE")){
+            if(answerText.contains("네")||answerText.contains("맞아")){
+                receive = new PlayerMP3(filePath + "접수_확인_여자.mp3");
+                answerText = "업무 접수 완료";
+                receive.playing();
+
+            }
+            else{
+
+                answerText = null;
+
+            }
+        }
+        else{
+            if(answerText.contains("네")||answerText.contains("맞아")){
+                receive = new PlayerMP3(filePath + "접수_확인_남자.mp3");
+                answerText = "업무 접수 완료";
+                receive.playing();
+            }
+            else {
+                answerText= null;
+
+            }
+        }
+
+        return answerText;
+    }
+    public String getTaks(){
+        return this.task;
+    }
+
+
+
+}
