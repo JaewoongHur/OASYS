9233d6e77f4f50861bf9f4d48ac317c9082dcf45
Crassula1994 <gogun3535@gmail.com>
2023-10-05 13:16:27 +0900

    Refactor: Modify icon name

--- frontend/oasys/src/assets/icons/index.ts
+++ frontend/oasys/src/assets/icons/index.ts
@@ -2,13 +2,13 @@
 import Bankbook from "./bankbook-icon.svg";
 import Card from "./card-icon.svg";
 import Internet from "./internet-icon.svg";
-import Money from "./money-icon.svg";
-import RightArrow from "./right-arrow-icon.svg";
 import MicOn from "./mic-on-icon.svg";
 import MicOff from "./mic-off-icon.svg";
+import Money from "./money-icon.svg";
+import RightArrow from "./right-arrow-icon.svg";
 import Wave from "./wave-background.svg";
 
 // ----------------------------------------------------------------------------------------------------
 
 /* Export */
-export { Bankbook, Card, Internet, Money, RightArrow, MicOn, MicOff, Wave };
+export { Bankbook, Card, Internet, MicOn, MicOff, Money, RightArrow, Wave };
