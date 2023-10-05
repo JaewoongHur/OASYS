6e2f5255ab1f7a92a848f160d7de9f50cd64854c
mj <aud19980518@gmail.com>
2023-10-05 15:23:42 +0900

    Feat: Update manager service for notification
    Feat: Update manager service for notification

--- Back-End/src/main/java/com/dolfin/oasys/service/ManagerServiceImpl.java
+++ Back-End/src/main/java/com/dolfin/oasys/service/ManagerServiceImpl.java
@@ -95,9 +95,7 @@ public class ManagerServiceImpl implements ManagerService {
         log.info("nextConsumerFaceId: " + nextFaceId);
 
         if (waitingList.size(WAITING + tellerType) >= 2) {
-            log.info(waitingList.index(WAITING + tellerType, 1));
-            log.info(String.valueOf(waitingList.index(WAITING + tellerType, 2).getClass()));
-            String faceIdForMessage = waitingList.index(WAITING + tellerType, 2);
+            String faceIdForMessage = waitingList.index(WAITING + tellerType, 1);
             log.info(faceIdForMessage);
             MemberDto.WaitingMember waitingMember = consumerInfoList.opsForValue().get(faceIdForMessage);
             MemberDto.ConsumerForMessage.builder()
