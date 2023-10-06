874775be957d5ff6c3a654c794e1bcc95cc3380e
Crassula1994 <gogun3535@gmail.com>
2023-10-06 15:04:33 +0900

    Refactor: Modify root page

--- frontend/oasys/src/main.tsx
+++ frontend/oasys/src/main.tsx
@@ -1,17 +1,22 @@
-// import React from "react";
-import ReactDOM from "react-dom/client";
+/* Import */
+import App from "@/App";
 import { Global, ThemeProvider } from "@emotion/react";
 import globalStyles from "@assets/styles/global";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import theme from "@assets/styles/theme";
-import App from "@/App";
 
+// ----------------------------------------------------------------------------------------------------
+
+/* Root Page */
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    // <React.StrictMode>
-    <>
-        <Global styles={globalStyles} />
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
-    </>,
-    // </React.StrictMode>,
+    <React.StrictMode>
+        <>
+            <Global styles={globalStyles} />
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </>
+        ,
+    </React.StrictMode>,
 );
